4e53bee408a42b0d29a23d7e8fbf83724d9726e0
danielrand <86839907+mynameisdanielrand@users.noreply.github.com>
2022-10-07 17:45:44 +0900

    z

--- hakaruweb/app.js
+++ hakaruweb/app.js
@@ -1,3 +1,11 @@
-const title = document.getElementById("title");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-title.innerText = "Hello guys";
+function handleTitleClick() {
+    if (h1.style.color === "blue") {
+        h1.style.color = "yellow";
+    } else {
+        h1.style.color = "blue";
+    }
+}
+
+h1.addEventListener("click", handleTitleClick);
