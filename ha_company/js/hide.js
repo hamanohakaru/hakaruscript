6494b4934ca570efb1c332d0a54117d01fcf5c03
danielrand <86839907+mynameisdanielrand@users.noreply.github.com>
2022-11-01 11:52:42 +0900

    Update hide.js

--- ha_company/js/hide.js
+++ ha_company/js/hide.js
@@ -17,6 +17,21 @@ $(document).ready(function () {
     /* 1 */
     $(window).scroll(function () {
         /* 2 */
+        $(".hideme2").each(function (i) {
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* 3 */
+            if (bottom_of_window > bottom_of_object / 2) {
+                $(this).animate({ opacity: "1" }, 1200);
+            }
+        });
+    });
+});
+
+$(document).ready(function () {
+    /* 1 */
+    $(window).scroll(function () {
+        /* 2 */
         $(".hideme .me_1").each(function (i) {
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
@@ -38,6 +53,36 @@ $(document).ready(function () {
             /* 3 */
             if (bottom_of_window > bottom_of_object / 2) {
                 $(this).animate({ opacity: "1" }, 3000);
+            }
+        });
+    });
+});
+
+$(document).ready(function () {
+    /* 1 */
+    $(window).scroll(function () {
+        /* 2 */
+        $(".hideme2 .me_3").each(function (i) {
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* 3 */
+            if (bottom_of_window > bottom_of_object / 2) {
+                $(this).animate({ opacity: "1" }, 7000);
+            }
+        });
+    });
+});
+
+$(document).ready(function () {
+    /* 1 */
+    $(window).scroll(function () {
+        /* 2 */
+        $(".hideme2 .me_4").each(function (i) {
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* 3 */
+            if (bottom_of_window > bottom_of_object / 2) {
+                $(this).animate({ opacity: "1" }, 8000);
             }
         });
     });
